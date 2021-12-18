be757918adc83773550ad7347a0a1e8c54df158a
FarhanaShaheed <farhana15-734@diu.edu.bd>
2021-12-18 23:31:13 +0600

    code added

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -7,7 +7,7 @@ import tw from 'tailwind-react-native-classnames';
 const Header = () => {
     return (
         <View style={tw`p-4 android:pt-2 text-white bg-blue-700 flex-row`}>
-        <Text style={tw`text-white text-2xl`}>Time Hack</Text>
+        <Text style={tw`text-white text-2xl`}>Time Zone</Text>
         </View>
     );
 };
